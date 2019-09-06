593780bb769d619fbf082a24ad4cc38b0ffed1a3
JS Morisset <jsm@surniaulula.com>
2019-09-06 06:56:27 -0700

    v2.8.0-rc.1

--- lib/register.php
+++ lib/register.php
@@ -111,9 +111,6 @@ if ( ! class_exists( 'WpssoJsonRegister' ) ) {
 				$clear_other = empty( $wpsso->options[ 'plugin_clear_on_activate' ] ) ? false : true;
 
 				$wpsso->util->schedule_clear_all_cache( $user_id = get_current_user_id(), $clear_other );
-
-			} else {
-				WpssoJson::required_notice( $deactivate = true );
 			}
 		}
 
