658aad56e1b0054d7cbe02c992a81a2fd0295b4a
JS Morisset <jsm@surniaulula.com>
2019-09-25 17:10:37 -0700

    v2.11.0-dev.4

--- lib/std/admin/meta-edit.php
+++ lib/std/admin/meta-edit.php
@@ -156,6 +156,7 @@ if ( ! class_exists( 'WpssoJsonStdAdminMetaEdit' ) ) {
 					'content'       => $form->get_no_textarea_value( $def_schema_desc, '', '', $schema_desc_max_len ),
 				),
 				'schema_addl_type_url' => array(
+					'tr_class' => $form->get_css_class_hide_prefix( 'basic', 'schema_addl_type_url' ),
 					'th_class' => 'medium',
 					'td_class' => 'blank',
 					'label'    => _x( 'Microdata Type URLs', 'option label', 'wpsso-schema-json-ld' ),
@@ -163,6 +164,7 @@ if ( ! class_exists( 'WpssoJsonStdAdminMetaEdit' ) ) {
 					'content'  => $form->get_no_input_value( '', 'wide', '', '', $repeat = 2 ),
 				),
 				'schema_sameas_url' => array(
+					'tr_class' => $form->get_css_class_hide_prefix( 'basic', 'schema_sameas_url' ),
 					'th_class' => 'medium',
 					'td_class' => 'blank',
 					'label'    => _x( 'Same-As URLs', 'option label', 'wpsso-schema-json-ld' ),
