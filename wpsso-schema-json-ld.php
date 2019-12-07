19a6f3338a36e1eca733571040fb22287a5ba1cd
JS Morisset <jsm@surniaulula.com>
2019-12-07 07:31:30 -0800

    v2.17.0-b.1

--- wpsso-schema-json-ld.php
+++ wpsso-schema-json-ld.php
@@ -11,6 +11,7 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Google Rich Results with Structured Data for Articles, Carousels, Events, FAQPages, HowTos, Images, Local Business, Products, Recipes, Reviews, Videos, and more.
+ * Requires PHP: 5.5
  * Requires At Least: 3.9
  * Tested Up To: 5.3
  * WC Tested Up To: 3.8.1
