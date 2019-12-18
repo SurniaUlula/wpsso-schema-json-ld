32a0def89d77cdaed25cb6bcb0914bc1b25b04cc
JS Morisset <jsm@surniaulula.com>
2019-12-18 10:10:08 -0800

    v2.17.2-b.1

--- wpsso-schema-json-ld.php
+++ wpsso-schema-json-ld.php
@@ -15,7 +15,7 @@
  * Requires At Least: 3.9
  * Tested Up To: 5.3.1
  * WC Tested Up To: 3.8.1
- * Version: 2.17.2-dev.1
+ * Version: 2.17.2-b.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
