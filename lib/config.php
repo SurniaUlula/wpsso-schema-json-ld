649469e4a3b269c8f41148e3cc016fec225699e6
JS Morisset <jsm@surniaulula.com>
2019-12-29 07:50:54 -0800

    v2.18.0-dev.1

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoJsonConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssojson' => array(			// Plugin acronym.
-					'version'     => '2.17.2',	// Plugin version.
+					'version'     => '2.18.0-dev.1',	// Plugin version.
 					'opt_version' => '38',		// Increment when changing default option values.
 					'short'       => 'WPSSO JSON',	// Short plugin name.
 					'name'        => 'WPSSO Schema JSON-LD Markup',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoJsonConfig' ) ) {
 					'req'         => array(
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
-						'min_version' => '6.16.1',
+						'min_version' => '6.16.2-dev.1',
 					),
 					'assets' => array(
 						'icons' => array(
--- lib/filters.php
+++ lib/filters.php
@@ -13,9 +13,10 @@ if ( ! class_exists( 'WpssoJsonFilters' ) ) {
 
 	class WpssoJsonFilters {
 
-		private $p;
-		private $msgs;
-		private $schema;
+		private $p;		// Wpsso class object.
+		private $msgs;		// WpssoJsonFiltersMessages class object.
+		private $schema;	// WpssoJsonFiltersSchema class object.
+		private $upg;		// WpssoJsonFiltersUpgrade class object.
 
 		public function __construct( &$plugin ) {
 
@@ -25,20 +26,33 @@ if ( ! class_exists( 'WpssoJsonFilters' ) ) {
 				$this->p->debug->mark();
 			}
 
+			/**
+			 * Instantiate the WpssoJsonFiltersSchema class object.
+			 */
 			if ( ! class_exists( 'WpssoJsonFiltersSchema' ) ) {
 				require_once WPSSOJSON_PLUGINDIR . 'lib/filters-schema.php';
 			}
 
 			$this->schema = new WpssoJsonFiltersschema( $plugin );
 
+			/**
+			 * Instantiate the WpssoJsonFiltersUpgrade class object.
+			 */
+			if ( ! class_exists( 'WpssoJsonFiltersUpgrade' ) ) {
+				require_once WPSSOJSON_PLUGINDIR . 'lib/filters-upgrade.php';
+			}
+
+			$this->upg = new WpssoJsonFiltersschema( $plugin );
+
 			$this->p->util->add_plugin_filters( $this, array(
-				'get_md_defaults'        => 2,
-				'rename_options_keys'    => 1,
-				'rename_md_options_keys' => 1,
+				'get_md_defaults' => 2,
 			) );
 
 			if ( is_admin() ) {
 
+				/**
+				 * Instantiate the WpssoJsonFiltersMessages class object.
+				 */
 				if ( ! class_exists( 'WpssoJsonFiltersMessages' ) ) {
 					require_once WPSSOJSON_PLUGINDIR . 'lib/filters-messages.php';
 				}
@@ -235,71 +249,6 @@ if ( ! class_exists( 'WpssoJsonFilters' ) ) {
 			$md_defs = array_merge( $md_defs, $schema_md_defs );
 
 			return $md_defs;
-		}
-
-		public function filter_rename_options_keys( $options_keys ) {
-
-			if ( $this->p->debug->enabled ) {
-				$this->p->debug->mark();
-			}
-
-			$options_keys[ 'wpssojson' ] = array(
-				16 => array(
-					'schema_def_course_provider_id' => 'schema_def_prov_org_id',
-				),
-			);
-
-			return $options_keys;
-		}
-
-		public function filter_rename_md_options_keys( $options_keys ) {
-
-			if ( $this->p->debug->enabled ) {
-				$this->p->debug->mark();
-			}
-
-			$options_keys[ 'wpssojson' ] = array(
-				11 => array(
-					'schema_event_org_id'  => 'schema_event_organizer_org_id',
-					'schema_event_perf_id' => 'schema_event_performer_org_id',
-					'schema_org_org_id'    => 'schema_organization_org_id',
-				),
-				14 => array(
-					'schema_event_place_id' => 'schema_event_location_id',
-					'schema_job_org_id'     => 'schema_job_hiring_org_id',
-				),
-				16 => array(
-					'schema_course_provider_id' => 'schema_prov_org_id',
-				),
-				20 => array(
-					'schema_question_desc' => 'schema_qa_desc',
-				),
-				26 => array(
-					'schema_part_of_url' => 'schema_ispartof_url',
-				),
-				32 => array(
-					'schema_review_claim_author_type'   => 'schema_review_item_cw_author_type',
-					'schema_review_claim_author_name'   => 'schema_review_item_cw_author_name',
-					'schema_review_claim_author_url'    => 'schema_review_item_cw_author_url',
-					'schema_review_claim_made_date'     => 'schema_review_item_cw_pub_date',
-					'schema_review_claim_made_time'     => 'schema_review_item_cw_pub_time',
-					'schema_review_claim_made_timezone' => 'schema_review_item_cw_pub_timezone',
-				),
-				34 => array(
-					'schema_review_item_book_isbn' => 'schema_review_item_cw_book_isbn',
-				),
-				35 => array(
-					'schema_review_item_product_img_id'     => 'schema_review_item_img_id',
-					'schema_review_item_product_img_id_pre' => 'schema_review_item_img_id_pre',
-					'schema_review_item_product_img_url'    => 'schema_review_item_img_url',
-				),
-				37 => array(
-					'schema_review_item_product_mpn' => 'schema_review_item_product_mfr_part_no',
-					'schema_review_item_product_sku' => 'schema_review_item_product_retailer_part_no',
-				),
-			);
-
-			return $options_keys;
 		}
 
 		public function filter_option_type( $type, $base_key ) {
@@ -574,6 +523,11 @@ if ( ! class_exists( 'WpssoJsonFilters' ) ) {
 
 					$this->p->notice->err( $notice_msg );
 				}
+			}
+
+			if ( $this->p->schema->is_schema_type_og_product( $md_opts[ 'schema_type' ] ) ) {
+				$md_opts[ 'og_type' ]    = 'product';
+				$md_opts[ 'og_type:is' ] = 'disabled';
 			}
 
 			return $md_opts;
