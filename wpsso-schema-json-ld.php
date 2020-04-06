e91ac55f7f8e5fabd41c13eaeafb99a649f7dbfb
JS Morisset <jsm@surniaulula.com>
2020-04-06 10:27:21 -0700

    v3.2.0

--- wpsso-schema-json-ld.php
+++ wpsso-schema-json-ld.php
@@ -15,7 +15,7 @@
  * Requires At Least: 4.2
  * Tested Up To: 5.4
  * WC Tested Up To: 4.0.1
- * Version: 3.2.0-rc.3
+ * Version: 3.2.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
@@ -185,6 +185,11 @@ if ( ! class_exists( 'WpssoJson' ) ) {
 			}
 
 			if ( ! $this->have_min_version ) {
+
+				if ( $this->p->debug->enabled ) {
+					$this->p->debug->log( 'exiting early: have_min_version is false' );
+				}
+
 				return;	// Stop here.
 			}
 
