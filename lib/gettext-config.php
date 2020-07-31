762e32dcc70f73f867988d23c8c6f34123b254b6
JS Morisset <jsm@surniaulula.com>
2020-07-31 07:08:52 -0700

    v3.14.0-dev.1

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -3,10 +3,6 @@
 /**
  * Extract of translatable text strings from the static config array.
  */
-_x( 'Edit Metabox Filters', 'lib file description', 'wpsso-schema-json-ld' );
-_x( 'Schema Markup Filters', 'lib file description', 'wpsso-schema-json-ld' );
-_x( 'Edit Metabox Filters', 'lib file description', 'wpsso-schema-json-ld' );
-_x( 'Schema Markup Filters', 'lib file description', 'wpsso-schema-json-ld' );
 _x( 'Schema Markup', 'lib file description', 'wpsso-schema-json-ld' );
 _x( 'Schema Shortcode Guide', 'lib file description', 'wpsso-schema-json-ld' );
 _x( 'Google Rich Results and Structured Data for Articles, Carousels (aka Item Lists), Claim Reviews, Events, FAQ Pages, How-Tos, Images, Local Business / Local SEO, Organizations, Products, Ratings, Recipes, Restaurants, Reviews, Videos, and More.', 'plugin description', 'wpsso-schema-json-ld' );
