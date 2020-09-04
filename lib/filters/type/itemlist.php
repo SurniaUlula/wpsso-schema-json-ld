c8ab9cbc3bcd85896b03bf2cfaff4447865b2b69
JS Morisset <jsm@surniaulula.com>
2020-09-04 10:11:17 -0700

    v4.1.0

--- lib/filters/type/itemlist.php
+++ lib/filters/type/itemlist.php
@@ -44,7 +44,7 @@ if ( ! class_exists( 'WpssoJsonFiltersTypeItemList' ) ) {
 
 			$ppp = SucomUtil::get_const( 'WPSSO_SCHEMA_ITEMS_PER_LIST_MAX', 100 );
 
-			WpssoSchema::add_itemlist_data( $json_data, $mod, $mt_og, $page_type_id, $is_main, $ppp );
+			$item_count = WpssoSchema::add_itemlist_data( $json_data, $mod, $mt_og, $page_type_id, $is_main, $ppp );
 
 			return $json_data;
 		}
