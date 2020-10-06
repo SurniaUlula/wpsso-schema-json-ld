9794ab91958dc5efeca69e3b61cc7e6bb4824374
JS Morisset <jsm@surniaulula.com>
2020-10-05 18:06:56 -0700

    v4.5.0-dev.1

--- wpsso-schema-json-ld.php
+++ wpsso-schema-json-ld.php
@@ -15,7 +15,7 @@
  * Requires At Least: 4.4
  * Tested Up To: 5.5.1
  * WC Tested Up To: 4.5.2
- * Version: 4.4.0
+ * Version: 4.5.0-dev.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
@@ -32,30 +32,32 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'These aren\'t the droids you\'re looking for.' );
 }
 
+if ( ! class_exists( 'SucomAddOn' ) ) {
+
+	require_once dirname( __FILE__ ) . '/lib/abstracts/com/add-on.php';	// SucomAddOn class.
+}
+
 if ( ! class_exists( 'WpssoJson' ) ) {
 
-	class WpssoJson {
-
-		/**
-		 * Wpsso plugin class object variable.
-		 */
-		public $p;		// Wpsso.
+	class WpssoJson extends SucomAddOn {
 
 		/**
 		 * Library class object variables.
 		 */
-		public $compat;		// WpssoJsonCompat (3rd party plugin and theme compatibility actions and filters).
-		public $conflict;	// WpssoJsonConflict (admin plugin conflict checks).
-		public $filters;	// WpssoJsonFilters.
-		public $reg;		// WpssoJsonRegister.
+		public $compat;		// WpssoJsonCompat class.
+		public $conflict;	// WpssoJsonConflict class.
+		public $filters;	// WpssoJsonFilters class.
+		public $reg;		// WpssoJsonRegister class.
 
 		/**
 		 * Reference Variables (config, options, modules, etc.).
 		 */
-		private static $ext           = 'wpssojson';
-		private static $p_ext         = 'json';
-		private static $missing_shown = false;
-		private static $instance      = null;
+		protected $p;
+		protected $ext   = 'wpssojson';
+		protected $p_ext = 'json';
+		protected $cf    = array();
+
+		private static $instance = null;
 
 		public function __construct() {
 
@@ -65,25 +67,27 @@ if ( ! class_exists( 'WpssoJson' ) ) {
 
 			WpssoJsonConfig::require_libs( __FILE__ );	// Includes the register.php class library.
 
+			$this->cf =& WpssoJsonConfig::$cf;
+
 			$this->reg = new WpssoJsonRegister();		// Activate, deactivate, uninstall hooks.
 
 			/**
 			 * WPSSO filter hooks.
 			 */
-			add_filter( 'wpsso_get_config', array( __CLASS__, 'wpsso_get_config' ), 30, 2 );
-			add_filter( 'wpsso_get_avail', array( __CLASS__, 'wpsso_get_avail' ), 20, 1 );
+			add_filter( 'wpsso_get_config', array( $this, 'get_config' ), 10, 1 );
+			add_filter( 'wpsso_get_avail', array( $this, 'get_avail' ), 10, 1 );
 
 			/**
 			 * WPSSO action hooks.
 			 */
-			add_action( 'wpsso_init_textdomain', array( __CLASS__, 'wpsso_init_textdomain' ) );
-			add_action( 'wpsso_init_objects', array( $this, 'wpsso_init_objects' ), 100 );
-			add_action( 'wpsso_init_plugin', array( $this, 'wpsso_init_plugin' ), 100 );
+			add_action( 'wpsso_init_textdomain', array( $this, 'init_textdomain' ), 1000, 1 );
+			add_action( 'wpsso_init_objects', array( $this, 'init_objects' ), 1000, 0 );
+			add_action( 'wpsso_init_plugin', array( $this, 'init_missing_requirements' ), 1000, 2 );
 
 			/**
 			 * WordPress action hooks.
 			 */
-			add_action( 'all_admin_notices', array( __CLASS__, 'maybe_show_notices' ) );
+			add_action( 'all_admin_notices', array( $this, 'show_missing_requirements' ) );
 		}
 
 		public static function &get_instance() {
@@ -96,54 +100,21 @@ if ( ! class_exists( 'WpssoJson' ) ) {
 			return self::$instance;
 		}
 
-		/**
-		 * Checks the core plugin version and merges the extension / add-on config array.
-		 */
-		public static function wpsso_get_config( $cf, $plugin_version = 0 ) {
+		public function init_textdomain( $debug_enabled = false ) {
 
-			if ( self::get_missing_requirements() ) {	// Returns false or an array of missing requirements.
+			static $local_cache = null;
 
-				return $cf;	// Stop here.
+			if ( null === $local_cache || $debug_enabled ) {
+
+				$local_cache = 'wpsso-schema-json-ld';
+
+				load_plugin_textdomain( 'wpsso-schema-json-ld', false, 'wpsso-schema-json-ld/languages/' );
 			}
 
-			return SucomUtil::array_merge_recursive_distinct( $cf, WpssoJsonConfig::$cf );
+			return $local_cache;
 		}
 
-		/**
-		 * The 'wpsso_get_avail' filter is run after the $check property is defined.
-		 */
-		public static function wpsso_get_avail( $avail ) {
-
-			if ( self::get_missing_requirements() ) {		// Returns false or an array of missing requirements.
-
-				$avail[ 'p_ext' ][ self::$p_ext ] = false;	// Signal that this extension / add-on is not available.
-
-				return $avail;
-			}
-
-			$avail[ 'p_ext' ][ self::$p_ext ] = true;		// Signal that this extension / add-on is available.
-
-			return $avail;
-		}
-
-		/**
-		 * The 'wpsso_init_textdomain' action is run after the $check, $avail, and $debug properties are defined.
-		 */
-		public static function wpsso_init_textdomain( $debug_enabled = false ) {
-
-			static $loaded = null;
-
-			if ( null !== $loaded && ! $debug_enabled ) {
-
-				return;
-			}
-
-			$loaded = true;
-
-			load_plugin_textdomain( 'wpsso-schema-json-ld', false, 'wpsso-schema-json-ld/languages/' );
-		}
-
-		public function wpsso_init_objects() {
+		public function init_objects() {
 
 			$this->p =& Wpsso::get_instance();
 
@@ -154,12 +125,7 @@ if ( ! class_exists( 'WpssoJson' ) ) {
 
 			$is_admin = is_admin();
 
-			if ( self::get_missing_requirements() ) {	// Returns false or an array of missing requirements.
-
-				if ( $this->p->debug->enabled ) {
-
-					$this->p->debug->log( 'exiting early: have missing requirements' );
-				}
+			if ( $this->get_missing_requirements() ) {	// Returns false or an array of missing requirements.
 
 				return;	// Stop here.
 			}
@@ -173,145 +139,6 @@ if ( ! class_exists( 'WpssoJson' ) ) {
 
 				$this->conflict = new WpssoJsonConflict( $this->p );	// Admin plugin conflict checks.
 			}
-		}
-
-		/**
-		 * All WPSSO objects are instantiated and configured.
-		 *
-		 * The $is_admin and $doing_ajax arguments are provided since WPSSO Core v7.4.0.
-		 */
-		public function wpsso_init_plugin( $is_admin = null, $doing_ajax = null ) {
-
-			$is_admin = null === $is_admin ? is_admin() : $is_admin;
-
-			$doing_ajax = null === $doing_ajax ? SucomUtil::get_const( 'DOING_AJAX' ) : $doing_ajax;
-
-			$missing_reqs = self::get_missing_requirements();	// Returns false or an array of missing requirements.
-
-			self::$missing_shown = true;
-
-			if ( ! $doing_ajax && $missing_reqs ) {
-
-				$error_pre = sprintf( '%s error:', __METHOD__ );
-
-				foreach ( $missing_reqs as $key => $req_info ) {
-
-					if ( ! empty( $req_info[ 'notice' ] ) ) {
-
-						if ( $is_admin ) {
-
-							$this->p->notice->err( $req_info[ 'notice' ] );
-
-							SucomUtil::safe_error_log( $error_pre . ' ' . $req_info[ 'notice' ], $strip_html = true );
-						}
-			
-						if ( $this->p->debug->enabled ) {
-
-							$this->p->debug->log( strtolower( $req_info[ 'notice' ] ) );
-						}
-					}
-				}
-
-				return;	// Stop here.
-			}
-		}
-
-		public static function maybe_show_notices() {
-
-			if ( self::$missing_shown ) {	// Nothing to do.
-
-				return;	// Stop here.
-			}
-
-			$missing_reqs = self::get_missing_requirements();	// Returns false or an array of missing requirements.
-
-			if ( ! $missing_reqs ) {
-
-				return;	// Stop here.
-			}
-
-			foreach ( $missing_reqs as $key => $req_info ) {
-
-				if ( ! empty( $req_info[ 'notice' ] ) ) {
-
-					echo '<div class="notice notice-error error"><p>';
-					echo $req_info[ 'notice' ];
-					echo '</p></div>';
-				}
-			}
-		}
-
-		/**
-		 * Returns false or an array of the missing requirements (ie. 'wpsso', 'woocommerce', etc.).
-		 */
-		private static function get_missing_requirements() {
-
-			static $local_cache = null;
-
-			if ( null !== $local_cache ) {
-
-				return $local_cache;
-			}
-
-			$local_cache = array();
-
-			$info = WpssoJsonConfig::$cf[ 'plugin' ][ self::$ext ];
-
-			foreach ( $info[ 'req' ] as $key => $req_info ) {
-
-				if ( ! empty( $req_info[ 'home' ] ) ) {
-
-					$req_name = '<a href="' . $req_info[ 'home' ] . '">' . $req_info[ 'name' ] . '</a>';
-
-				} else {
-
-					$req_name = $req_info[ 'name' ];
-				}
-
-				if ( ! empty( $req_info[ 'version_global' ] ) && ! empty( $GLOBALS[ $req_info[ 'version_global' ] ] ) ) {
-
-					$req_info[ 'version' ] = $GLOBALS[ $req_info[ 'version_global' ] ];
-
-				} elseif ( ! empty( $req_info[ 'version_const' ] ) && defined( $req_info[ 'version_const' ] ) ) {
-
-					$req_info[ 'version' ] = constant( $req_info[ 'version_const' ] );
-
-				} elseif ( ! empty( $req_info[ 'plugin_class' ] ) && ! class_exists( $req_info[ 'plugin_class' ] ) ) {
-
-					self::wpsso_init_textdomain();	// If not already loaded, load the textdomain now.
-
-					$notice_msg = __( 'The %1$s version %2$s add-on requires the %3$s plugin &mdash; please activate the missing plugin.', 'wpsso-schema-json-ld' );
-
-					$req_info[ 'notice' ] = sprintf( $notice_msg, $info[ 'name' ], $info[ 'version' ], $req_name );
-				}
-
-				if ( ! empty( $req_info[ 'version' ] ) ) {
-
-					if ( ! empty( $req_info[ 'min_version' ] ) ) {
-
-						if ( version_compare( $req_info[ 'version' ], $req_info[ 'min_version' ], '<' ) ) {
-
-							self::wpsso_init_textdomain();	// If not already loaded, load the textdomain now.
-
-							$notice_msg = __( 'The %1$s version %2$s add-on requires %3$s version %4$s or newer (version %5$s is currently installed).', 'wpsso-schema-json-ld' );
-
-							$req_info[ 'notice' ] = sprintf( $notice_msg, $info[ 'name' ], $info[ 'version' ], $req_name, $req_info[ 'min_version' ], $req_info[ 'version' ] );
-						}
-					}
-				}
-
-				if ( ! empty( $req_info[ 'notice' ] ) ) {
-
-					$local_cache[ $key ] = $req_info;
-				}
-			}
-
-			if ( empty( $local_cache ) ) {
-
-				$local_cache = false;
-			}
-
-			return $local_cache;
 		}
 	}
 
